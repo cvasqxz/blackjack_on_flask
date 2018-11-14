bd8818132c10663b63f0ad6501c127348b30ff74
heellxz <heellxz@gmail.com>
2018-11-14 12:28:35 -0300

    Py3 algo fix

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, url_for
 import random
 from database import db_session, init_db
 from models import Deck, UserHand, ComputerHand
@@ -7,15 +7,19 @@ from models import Deck, UserHand, ComputerHand
 app = Flask(__name__)
 
 CARD_SCORES = {
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
     '6': 6,
     '7': 7,
     '8': 8,
     '9': 9,
     '10': 10,
-    'Valet': 2,
-    'Dama': 3,
-    'Korol': 4,
-    'Tuz': 11
+    'J': 10,
+    'Q': 10,
+    'K': 10,
+    'A': 11
 }
 
 
@@ -40,9 +44,11 @@ def start_game():
 @app.route('/get_card')
 def get_card():
     add_user_card(get_card_from_deck())
-    computer_score = calc_score(get_computer_cards())
-    if computer_score < 17 or (21 - computer_score > 2):  # Computer should get card in case of it's score < 17
-        add_computer_card(get_card_from_deck())
+    user_score = calc_score(get_user_cards())
+
+    if user_score > 21:
+        return redirect(url_for('stop'))
+
     user_cards = get_user_cards()
     return render_template('game.jinja2', user_cards=user_cards, user_score=calc_score(user_cards))
 
@@ -50,7 +56,18 @@ def get_card():
 @app.route('/stop')
 def stop():
     user_score = calc_score(get_user_cards())
+    user_cards = get_user_cards()
+
     computer_score = calc_score(get_computer_cards())
+
+    while computer_score < 17:
+        add_computer_card(get_card_from_deck())
+        computer_score = calc_score(get_computer_cards())
+        if user_score > 21 or computer_score > user_score:
+            break
+
+    computer_cards = get_computer_cards()
+
     if (user_score > 21 and computer_score > 21) or user_score == computer_score:
         result = 'Friendship won'
     elif user_score <= 21 and computer_score > 21:
@@ -61,7 +78,7 @@ def stop():
         result = 'You won'
     else:
         result = 'You lose'
-    return render_template('score.jinja2', result=result, computer_score=computer_score, user_score=user_score)
+    return render_template('score.jinja2', result=result, computer_score=computer_score, computer_cards=computer_cards, user_cards=user_cards, user_score=user_score)
 
 
 def init_or_flush_cards():
@@ -78,7 +95,10 @@ def generate_deck():
     '''
     Generates list of cards and stores it to model
     '''
-    deck = [a for a in CARD_SCORES.keys()] * 4
+    deck = []
+    types = ['S', 'C', 'D', 'H']
+    for i in types:
+        deck += [a+i for a in CARD_SCORES.keys()]
     random.shuffle(deck, random.random)
     for card in deck:
         card = Deck(str(card))
@@ -126,7 +146,7 @@ def save():
     try:
         db_session.commit()
     except Exception as e:
-        print e
+        print(e)
 
 
 def calc_score(cards):
@@ -136,15 +156,15 @@ def calc_score(cards):
     '''
     score = 0
     for card in cards:
-        score += CARD_SCORES[str(card)]
+        score += CARD_SCORES[str(card).rstrip('SCDH')]
     return score
 
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
     db_session.remove()
-    print exception
+    print(exception)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
